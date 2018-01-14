509e12b79dcc8e7f373fc8a4baac7a9e58380e3f
rightmeow <jinhedev@gmail.com>
2018-01-14 12:24:48 -0800

    added support for enter key for text submission and used ajax to save the frontend from running another page reload

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  
+
   before_action :logged_in_user
   before_action :get_messages
 
@@ -9,9 +9,7 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
-      redirect_to messages_url
-    else
-      render 'index'
+      ActionCable.server.broadcast 'room_channel', content: message.content, username: message.user.username
     end
   end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # add heroku / aws / localhost origin for ActionCable requests.
+  config.action_cable.allowed_request_origins = [
+    'localhost:3000'
+  ]
 end
