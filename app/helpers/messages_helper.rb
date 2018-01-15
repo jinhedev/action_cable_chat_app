496853e6e95d6a4c90576bee21461f90345fde99
rightmeow <jinhedev@gmail.com>
2018-01-14 21:09:51 -0800

    added support for @mention

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,8 @@
 module MessagesHelper
+
+  # returns html from input text using GitHub-flavoured Markdown.
+  def markdown_to_html(text)
+    Kramdown::Document.new(text, input: 'GFM').to_html
+  end
+
 end
